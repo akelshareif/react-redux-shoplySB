5e032e1bbc494604934a73fe05f9e003167a5833
Abdelkareem ElShareif <abdelkareemelshareif@abdelkareemsmbp.lan>
2020-11-06 15:12:37 -0500

    initial boilerplate setup added

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = { something: '' };
+
+const rootReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default rootReducer;
